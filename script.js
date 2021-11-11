09f9272e1f9ce298a6dab583ecd5f516b5294cf4
Pawel Michalski <pawelmm96@gmail.com>
2021-11-11 22:41:58 +0100

    fix animations

--- script.js
+++ script.js
@@ -20,11 +20,15 @@ const releaseSpring = () => {
     btn.textContent = `force: ${progressPercent}%`
 
     document.documentElement.style.setProperty('--power-x', `${progressPercent * 7 + 120}px`);
-    ball.style.animation = "fly-ball-x 2s .2s cubic-bezier(.17,.67,.6,1),fly-ball-y 2s .2s linear";
-
+    ball.style.animation = "fly-ball-x 2s 1 forwards .2s cubic-bezier(.17,.67,.6,1),fly-ball-y 2s 1 forwards .2s linear";
 
     document.documentElement.style.setProperty('--spring-left', parseInt(getComputedStyle(spring).left, 10));
     spring.style.animation = 'release-spring .2s ease-in';
+
+    btn.removeEventListener('mousedown', stretchSpring)
+    btn.removeEventListener('mouseup', releaseSpring)
+    btn.removeEventListener('touchstart', stretchSpring)
+    btn.removeEventListener('touchend', releaseSpring)
 }
 const resetAnimation = () => {
     console.log('reset animacji');
